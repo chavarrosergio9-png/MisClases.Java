c492c110c22b6397062b6f706f5ca26308758714
Sergio <chavarrosergio9@gmail.com>
2026-08-28 19:40:13 -0500

    Agregar avance del dia

---
+++ CalculadoraAsteriscos.java
@@ -0,0 +1,101 @@
+// ciclos anidados( cuando algo va adentro de algo)
+import java.util.Scanner;
+public class CalculadoraAsteriscos {
+public static void main(String[] args){
+ /*   for (int fila = 1; fila <= 4; fila++){
+        for (int col = 1; col <= 4; col++){
+            System.out.print("*");
+        }
+        System.out.println();
+    } */
+
+ /*   for ( int i = 1; i <= 10 ; i++){
+        if (i == 5) break;
+        System.out.println(i);
+    } */
+
+     /*   for ( int i = 1; i <= 10 ; i++){
+                if (i % 2 == 0) continue;
+                System.out.println(i);
+            }
+      */
+     
+   /* int contador =1;
+    while (contador <= 5){
+        System.out.println("Vuelta número " + contador);
+        contador++;
+
+    }
+     */
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    /*
+    while ( edad < 0 || edad > 120){
+        System.out.println("Ingresa tu edad (0-120): ");
+        edad = sc.nextInt();
+        if (edad < 0 || edad > 120){
+            System.out.println("Edad inválida, intenta de nuevo");
+        }
+
+
+    }
+    System.out.println("Tu edad es: " + edad);
+    sc.close();
+    
+    
+do{
+    System.out.println("/n=== MENÚ ===");
+    System.out.println("1. SALUDAR");
+    System.out.println("2. DESPEDIRSE");
+    System.out.println("3. SALIR");
+    option = sc.nextInt();
+
+    switch (option) {
+        case 1:
+            System.out.println("¡Hola!");
+            break;
+        case 2:
+            System.out.println("¡Chao!");
+            break;
+        case 3:
+            System.out.println("¡Hasta luego!");
+            break;
+        default:
+            System.out.println("Opción inválida, intenta de nuevo");
+
+    }
+}
+        
+while (option != 3);
+    sc.close();
+     */
+
+   /* int [] notas = new int[5];
+    notas[0] = 85;
+    notas[1] = 90;
+    notas[2] = 78;
+    notas[3] = 92;
+    notas[4] = 88;
+    int [] edades ={18, 20, 22, 19, 21};
+    System.out.println(notas[0]);
+    for (int i = 0; i < notas.length; i ++){
+        System.out.println("Nota " + (i+1) + " : " +  notas[i]);
+
+    }
+ */
+
+    double [] notas = new double[n];
+    double suma = 0;
+    for ( int i = 0; i < ("notas del estudiante" + (i + 1) + ":");
+    
+
+
+    
+
+
+}
+}
+
+
+
